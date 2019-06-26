36460f9b813d74990867771c5e168c58124bb556
jatin verma <jatin_verma@outlook.com>
2019-06-25 20:46:41 -0400

    Initial Commit

---
+++ CelebFan/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CelebFan
+//
+//  Created by MacStudent on 2019-06-25.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
